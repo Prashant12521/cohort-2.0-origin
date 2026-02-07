fb9b813bf20424e43495f5d8a9bbb3e0bfce5ac1
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-07 16:10:05 +0530

    day-13: setup auth flow using jwt and cookies

---
+++ day-13-me/src/routes/auth.routes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const crypto = require('crypto')
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isAccountAlreadyExists = await userModel.findOne({ email });
+
+  if (isAccountAlreadyExists) {
+    res.status(409).json({
+      message: "user already exists with this email",
+    });
+  }
+
+  const hash = crypto.createHash('md5').update(password).digest('hex')
+
+  const user = await userModel.create({
+    name,
+    email,
+    password:hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "user registered",
+    user,
+    token,
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "user not found with this email",
+    });
+  }
+
+  const checkPassword = user.password === crypto.createHash('md5').update(password).digest('hex');
+
+  if (!checkPassword) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "login successful",
+    user,
+  });
+});
+
+module.exports = authRouter;

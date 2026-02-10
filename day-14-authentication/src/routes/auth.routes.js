e8fdfc127a446017ae65c441b70cb8914dadf084
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-10 20:12:24 +0530

    day-14: Authentication

---
+++ day-14-authentication/src/routes/auth.routes.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isUserExists = await userModel.findOne({ email });
+
+  if (isUserExists) {
+    return res.status(409).json({
+      message: "User already exist",
+    });
+  }
+
+  const user = await userModel.create({
+    name,
+    email,
+    password: crypto.createHash("sha256").update(password).digest("hex"),
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
+
+authRouter.get("/get-me", async (req, res) => {
+  const token = req.cookies.token;
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  // console.log(decoded)
+
+  const user = await userModel.findById(decoded.id);
+
+  res.json({
+    name: user.name,
+    email: user.email,
+  });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const isPasswordValid = hash === user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User logged in successfully",
+    user: {
+      name: user.name,
+      email: user.email,
+    },
+  });
+});
+
+module.exports = authRouter;

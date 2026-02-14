b2e3bae2b2fd54408f73d709e950f3c4f8661df2
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-14 20:51:55 +0530

    day-16-me3

---
+++ day-16-me3/src/controllers/post.controller.js
@@ -0,0 +1,49 @@
+const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+const jwt = require('jsonwebtoken')
+
+const client = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+async function createPostController(req, res) {
+  console.log(req.body, req.file);
+
+  const token = req.cookies.token
+
+  if(!token){
+    return res.status(401).json({
+      message:'token not found; unauthorized access'
+    })
+  }
+
+  let decoded = null
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
+  } catch (error) {
+    res.status(401).json({
+      message: 'jwt verification failed'
+    })
+  }
+
+  const file = await client.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer),'file'),
+    fileName: 'vscodeImage',
+    folder: 'cohort-2-insta-clone-posts'
+  })
+
+  const post = await postModel.create({
+    caption:req.body.caption,
+    imgUrl:file.url,
+    user:decoded.id
+  })
+
+  res.status(201).json({
+    message:'post created successfully'
+  })
+
+}
+
+module.exports = { createPostController };
---
+++ day-16-me3/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username:{
+    type:String,
+    unique:[true, 'username already used'],
+    required:[true, 'username required']
+  },
+  email:{
+    type:String,
+    unique:[true, 'email already used'],
+    required:[true, 'email required']
+  },
+  password:{
+    type:String,
+    required:[true, 'password required']
+  },
+  bio:String,
+  profileImage:{
+    type:String,
+    default:'https://ik.imagekit.io/cc5gkipl0/default-avatar-social-media-display-600nw-2632690107.webp?updatedAt=1770890981198'
+  }
+})
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel

73834ec8c38dd06bab34dcda558759087af2255b
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-15 09:15:59 +0530

    day-17: project-1-continue (add GET Method - getPost & getPostDetails)

--- day-16-me3/src/controllers/post.controller.js
+++ day-16-me3/src/controllers/post.controller.js
@@ -1,7 +1,7 @@
 const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -10,40 +10,109 @@ const client = new ImageKit({
 async function createPostController(req, res) {
   console.log(req.body, req.file);
 
-  const token = req.cookies.token
+  const token = req.cookies.token;
 
-  if(!token){
+  if (!token) {
     return res.status(401).json({
-      message:'token not found; unauthorized access'
-    })
+      message: "token not found; unauthorized access",
+    });
   }
 
-  let decoded = null
+  let decoded = null;
 
   try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET)
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (error) {
     res.status(401).json({
-      message: 'jwt verification failed'
-    })
+      message: "jwt verification failed",
+    });
   }
 
   const file = await client.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer),'file'),
-    fileName: 'vscodeImage',
-    folder: 'cohort-2-insta-clone-posts'
-  })
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "vscodeImage",
+    folder: "cohort-2-insta-clone-posts",
+  });
 
   const post = await postModel.create({
-    caption:req.body.caption,
-    imgUrl:file.url,
-    user:decoded.id
-  })
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id,
+  });
 
   res.status(201).json({
-    message:'post created successfully'
+    message: "post created successfully",
+  });
+}
+
+async function getPostController(req, res) {
+  const token = req.cookies.token;
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token invalid",
+    });
+  }
+
+  const userId = decoded.id;
+
+  const posts = await postModel.find({
+    user: userId,
+  });
+
+  res.status(200).json({
+    message: "Post fetched successfully",
+    posts,
+  });
+}
+
+async function getPostDetailsController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "UnAuthorized Access",
+    });
+  }
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+
+  const userId = decoded.id;
+  const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(404).json({
+      message: "Post not found.",
+    });
+  }
+
+  const isValidUser = post.user.toString() === userId;
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden Content.",
+    });
+  }
+
+  return res.status(200).json({
+    message: 'Post fetched successfully.',
+    post
   })
 
 }
 
-module.exports = { createPostController };
+module.exports = { createPostController, getPostController, getPostDetailsController };
--- day-16-me3/src/routes/post.routes.js
+++ day-16-me3/src/routes/post.routes.js
@@ -5,6 +5,15 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const postRouter = express.Router();
 
-postRouter.post("/", upload.single('image'), postController.createPostController);
+postRouter.post("/", upload.single("image"), postController.createPostController);
+
+// GET - /api/posts/ [protected]
+
+postRouter.get("/", postController.getPostController);
+
+// GET - /api/posts/details/:postid
+// - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+
+postRouter.get('/details/:postId', postController.getPostDetailsController)
 
 module.exports = postRouter;

aabcd89f5332f0d87a74caa047f92956f001ec61
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-16 22:02:34 +0530

    day-18: project-1-continue (add follow model with follower and followee references)

---
+++ day-17-me/src/models/follow.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "Follower is required"],
+    },
+    followee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "Follower is required"],
+    },
+  },
+  { timestamps: true },
+);
--- day-17-me/src/models/user.model.js
+++ day-17-me/src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
   profileImage:{
     type:String,
     default:'https://ik.imagekit.io/cc5gkipl0/default-avatar-social-media-display-600nw-2632690107.webp?updatedAt=1770890981198'
-  }
+  },
 })
 
 const userModel = mongoose.model('users', userSchema)

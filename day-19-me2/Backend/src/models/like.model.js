37e53315f60e823e83bea31291ab244da325c836
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-02-19 16:17:14 +0530

    day-19-me2 (Backend)

---
+++ day-19-me2/Backend/src/models/like.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const likeSchema = new mongoose.Schema(
+  {
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "posts",
+      required: [true, "post id is required"],
+    },
+    user: {
+      type: String,
+      required: [true, "username is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+likeSchema.index({ username: 1, postId: 1 }, { unique: true });
+
+const likeModel = mongoose.model("like", likeSchema);
+
+module.exports = likeModel;

0c9bc3f6d5b39a1adc83783fcc73ff58e9ca16e5
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-03-01 10:11:16 +0530

    day-27: add user authentication and session management with token blacklisting

--- day-25/Backend/src/routes/auth.routes.js
+++ day-25/Backend/src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 const {Router} = require('express')
 const authController = require('../controllers/auth.controller')
+const authMiddleWare = require('../middleware/auth.middleware')
 
 const router = Router()
 
 router.post('/register', authController.registerUser)
 router.post('/login', authController.loginUser)
+
+router.get('/get-me', authMiddleWare.authUser, authController.getMe)
+
+router.get('/logout', authController.logoutUser)
 
 module.exports = router

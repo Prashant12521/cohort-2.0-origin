2ae748293b3c81f08b696cb4810b5761421322c3
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-03-01 19:29:32 +0530

    day-28: integrate Redis for token management and update authentication middleware

--- day-25/Backend/src/controllers/auth.controller.js
+++ day-25/Backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const blacklistingModel = require("../models/blacklist.model");
+const redis = require("../config/cache");
 
 async function registerUser(req, res) {
   const { username, email, password } = req.body;
@@ -49,26 +50,25 @@ async function registerUser(req, res) {
 async function loginUser(req, res) {
   const { email, password, username } = req.body;
 
-  const user = await userModel.findOne({
-    $or:[
-      {email},
-      {username}
-    ]
-  }).select('+password')
-
-  if(!user){
-    return res.status(400).json({
-      message: 'Invalid credentials'
-      // user not found ham kyo nahi de rahe
+  const user = await userModel
+    .findOne({
+      $or: [{ email }, { username }],
     })
+    .select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+      // user not found ham kyo nahi de rahe
+    });
   }
 
-  const isPasswordValid = await bcrypt.compare(password, user.password)
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  if(!isPasswordValid){
+  if (!isPasswordValid) {
     return res.status(400).json({
-      message: 'Invalid credentials'
-    })
+      message: "Invalid credentials",
+    });
   }
 
   const token = jwt.sign(
@@ -93,27 +93,25 @@ async function loginUser(req, res) {
   });
 }
 
-async function getMe(req,res) {
-  const user = await userModel.findById(req.user.id)
+async function getMe(req, res) {
+  const user = await userModel.findById(req.user.id);
 
   res.status(200).json({
-    message: 'User fetched successfully',
-    user
-  })
+    message: "User fetched successfully",
+    user,
+  });
 }
 
-async function logoutUser(req,res) {
-  const token = req.cookies.token
+async function logoutUser(req, res) {
+  const token = req.cookies.token;
 
-  res.clearCookie('token')
+  res.clearCookie("token");
 
-  await blacklistingModel.create({
-    token
-  })
+  await redis.set(token, Date.now().toString(), "EX", 60 * 60);
 
   res.status(201).json({
-    message: 'logout successfully'
-  })
+    message: "logout successfully",
+  });
 }
 
 module.exports = { registerUser, loginUser, getMe, logoutUser };

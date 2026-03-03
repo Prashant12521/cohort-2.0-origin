8beb2c437dd5547b46a8776703aac9494e873b97
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-03-03 16:35:38 +0530

    day-29: react 4 layer architecture in action

--- day-25/Backend/src/app.js
+++ day-25/Backend/src/app.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const router = require('./routes/auth.routes')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const router = require("./routes/auth.routes");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }));
 
-app.use('/api/auth', router)
+app.use("/api/auth", router);
 
-
-module.exports = app
+module.exports = app;

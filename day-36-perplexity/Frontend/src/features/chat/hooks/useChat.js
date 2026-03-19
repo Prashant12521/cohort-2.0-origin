eeea7af30679d475853547814e39099004d552bd
Prashant Kumar <prashantkumarbarnwal120@gmail.com>
2026-03-19 20:51:54 +0530

    day:39-perplexity: add react-markdown for rendering messages and enhance chat functionality

--- day-36-perplexity/Frontend/src/features/chat/hooks/useChat.js
+++ day-36-perplexity/Frontend/src/features/chat/hooks/useChat.js
@@ -1,34 +1,72 @@
-import { initializeSocketConnection } from "../service/chat.socket.js";
-import { sendMessage, getChats, getMessage, deleteChat } from "../service/chat.api.js";
-import { setChats, setCurrentChatId, setError, setLoading, createNewChat, addNewMessage } from '../chat.slice.js';
-import {useDispatch} from 'react-redux';
+import { initializeSocketConnection } from "../service/chat.socket";
+import { sendMessage, getChats, getMessage, deleteChat } from "../service/chat.api";
+import { setChats, setCurrentChatId, setError, setLoading, createNewChat, addNewMessage, addMessages } from "../chat.slice";
+import { useDispatch } from "react-redux";
+
 
 export const useChat = () => {
-  const dispatch = useDispatch();
 
-  async function handleSendMessage({ message, chatId }) {
-    dispatch(setLoading(true));
-    const data = await sendMessage({ message, chatId });
-    const { chat, aiMessage } = data;
-    dispatch(createNewChat({
-      chatId: chat._id,
-      title:chat.title
-    }));
-    dispatch(addNewMessage({
-      chatId: chat._id,
-      content: message,
-      role: 'user'
-    }))
-    dispatch(addNewMessage({
-      chatId: chat._id,
-      content: aiMessage.content,
-      role: aiMessage.role
-    }))
-    dispatch(setCurrentChatId(chat._id))
-  }
+    const dispatch = useDispatch()
 
-  return {
-    initializeSocketConnection,
-    handleSendMessage,
-  };
-};
+
+    async function handleSendMessage({ message, chatId }) {
+        dispatch(setLoading(true))
+        const data = await sendMessage({ message, chatId })
+        const { chat, aiMessage } = data
+        dispatch(createNewChat({
+            chatId: chat._id,
+            title: chat.title,
+        }))
+        dispatch(addNewMessage({
+            chatId: chat._id,
+            content: message,
+            role: "user",
+        }))
+        dispatch(addNewMessage({
+            chatId: chat._id,
+            content: aiMessage.content,
+            role: aiMessage.role,
+        }))
+        dispatch(setCurrentChatId(chat._id))
+    }
+
+    async function handleGetChats() {
+        dispatch(setLoading(true))
+        const data = await getChats()
+        const { chats } = data
+        dispatch(setChats(chats.reduce((acc, chat) => {
+            acc[ chat._id ] = {
+                id: chat._id,
+                title: chat.title,
+                messages: [],
+                lastUpdated: chat.updatedAt,
+            }
+            return acc
+        }, {})))
+        dispatch(setLoading(false))
+    }
+
+    async function handleOpenChat(chatId) {
+
+        const data = await getMessage(chatId)
+        const { messages } = data
+
+        const formattedMessages = messages.map(msg => ({
+            content: msg.content,
+            role: msg.role,
+        }))
+        dispatch(addMessages({
+            chatId,
+            messages: formattedMessages,
+        }))
+        dispatch(setCurrentChatId(chatId))
+    }
+
+    return {
+        initializeSocketConnection,
+        handleSendMessage,
+        handleGetChats,
+        handleOpenChat
+    }
+
+}
